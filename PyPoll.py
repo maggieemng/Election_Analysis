f754e36eea6e22d138c0e3982578b32317a4a384
Maggie M <mm@Maggies-MBP.cable.rcn.com>
2022-03-13 19:54:08 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -28,6 +28,12 @@ candidate_options = []
 # Declare the empty dictionary.
 candidate_votes = {}
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -51,30 +57,6 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
-
-# Print the total votes.
-print(total_votes)
-
-# Print the candidate list.
-print(candidate_options)
-
-# Print the candidate vote dictionary.
-print(candidate_votes)
-
-# Determine the percentage of votes for each candidate by looping through the counts.
-# 1. Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
-
-# Winning Candidate and Winning Count Tracker
-winning_candidate = ""
-winning_count = 0
-winning_percentage = 0
 
 # Determine the percentage of votes for each candidate by looping through the counts.
 # Iterate through the candidate list.
